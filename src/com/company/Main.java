737a6022d03d4d4a57f87dfc044dbbfb69dd64c4
cristianabote <cristianamihaelaplesa@gmail.com>
2021-03-29 20:12:06 +0300

    3 commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,18 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         // Fie o multime de N cutii numerotate de la 1 la N.Fiecare cutie este caracterizata de un id(nr scris pe ea),
-            //latime, lungime si inaltime.Cutiile sunt simulate folosind clasa Box.
-            //*Implementati constructorul pentru aceasta clasa.
-            //*Implementati o metoda ce intoarce volumul cutiei.
+        //latime, lungime si inaltime.Cutiile sunt simulate folosind clasa Box.
+        //*Implementati constructorul pentru aceasta clasa.
+        //*Implementati o metoda ce intoarce volumul cutiei.
         //*Verificati daca putem pune toate cutiile una in interiorul celeilalte astfel incat la final sa avem o singura
         //cutie .Daca se poate printati ordinea cutiilor, de la exterior spre interior, folosind formatul dat.
         //In caz contrar, printati "Nu se poate".
 
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("Introduceti numarul de cutii: ");
-        int nrBox=scan.nextInt();
-        Box [] boxCollection=new Box[nrBox];
-        for(int i=0; i<nrBox;i++) { //citim de la tastatura detaliile despre cele N cutii
+        int nrBox = scan.nextInt();
+        Box[] boxCollection = new Box[nrBox];
+        for (int i = 0; i < nrBox; i++) { //citim de la tastatura detaliile despre cele N cutii
             System.out.println("Introduceti id cutiei " + (i + 1));
             int id = scan.nextInt();
             System.out.println("Introduceti latimea cutiei " + (i + 1));
@@ -34,20 +34,22 @@ public class Main {
             System.out.println(" ");
         }
         Arrays.sort(boxCollection);
-        int found=0;
-        Box current=boxCollection[0];
-        for(int i=0; i<nrBox&&found==0;i++) {
-            System.out.println(boxCollection[i].toString());
+        int found = 0;
+        Box current = boxCollection[0];
+        for (int i = 1; i < nrBox && found == 0; i++) {
+            // System.out.println(boxCollection[i].toString());
             if ((current.getLatime() < boxCollection[i].getLatime()) || (current.getInaltime() < boxCollection[i].getInaltime())) {
                 found = 1;
                 current = boxCollection[i];
             }
         }
-        if(found==1){
+        if (found == 1) {
             System.out.println("Nu se pot introduce cutiile unele in celelalte.");
-        }else{
-            System.out.println("Cutiile intra una in cealalta iar ordinea lor este cea de mai sus. ");
+        } else {
+            System.out.println("Cutiile intra una in cealalta iar ordinea lor este cea de mai jos: ");
+            for (int i = 0; i < nrBox; i++) {
+                System.out.println(boxCollection[i]);
             }
         }
     }
-
+    }
